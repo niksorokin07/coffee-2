ce0908e8527bdb3bad98ddfc88dd17411c1324ab
niksorokin07 <117514209+niksorokin07@users.noreply.github.com>
2022-11-19 17:55:15 +0300

    main

---
+++ main.py
@@ -0,0 +1,35 @@
+import sqlite3
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class DBSample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.btn.clicked.connect(self.select_data)
+        self.cur = self.connection.cursor()
+        self.result = self.cur.execute(f"SELECT * FROM Coffee").fetchall()
+
+    def select_data(self):
+        title = [el[0] for el in self.cur.description]
+        self.tableWidget.setVerticalHeaderLabels(title)
+        self.tableWidget.setColumnCount(len(title))
+        self.tableWidget.setHorizontalHeaderLabels(title)
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(self.result):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.tableWidget.resizeColumnsToContents()
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = DBSample()
+    ex.show()
+    sys.exit(app.exec())
